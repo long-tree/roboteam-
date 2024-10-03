ad0e764b0d9a501a7e495f8903b225e6e9ca0e10
ng <3076877237@qq.com>
2024-10-03 21:28:53 +0800

    sec commit

---
+++ build/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/a.c.o"
+  "CMakeFiles/a.out.dir/a.c.o.d"
+  "CMakeFiles/a.out.dir/b.c.o"
+  "CMakeFiles/a.out.dir/b.c.o.d"
+  "a.out"
+  "a.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
